611681b8193e36e6e7e0367f2f1cc8c487fa235c
lopeselio <lopeselio@gmail.com>
2021-08-03 18:26:32 +0530

    Theme

---
+++ src/theme.js
@@ -0,0 +1,34 @@
+import { extendTheme, withDefaultColorScheme } from "@chakra-ui/react";
+
+const Theme = extendTheme({ 
+    fonts: {
+        heading: "Inter",
+        body: "Inter"
+    },
+    components: {
+        Text: {
+            baseStyle: {
+                fontWeight: "semibold",
+                fontFamily: "Inter"
+            }
+        },
+        Tag: {
+            baseStyle: {
+                background: "rgba(230,1,122,0.08)",
+                color: "#E6017A"
+            }
+        },
+        Button: {
+            baseStyle: {
+                fontFamily: "Inter"
+            }
+        },
+        Heading: {
+            baseStyle: {
+                fontFamily: "Inter"
+            }
+        }
+    }
+});
+
+export default Theme;
