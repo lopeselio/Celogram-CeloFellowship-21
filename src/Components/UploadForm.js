bbc2b4cd049b49cb96bf231cdac1d5db612a4d74
lopeselio <lopeselio@gmail.com>
2021-08-11 03:08:46 +0530

    upload form

--- src/Components/UploadForm.js
+++ src/Components/UploadForm.js
@@ -27,7 +27,7 @@ import {
   
 
 class Upload extends Component {
-      state = {currentImage:"https://iconscout.com/icon/image-1889136"}
+      state = {currentImage:'https://cdn.iconscout.com/icon/free/png-256/gallery-187-902099.png'}
 
       imageHandler = (e) => {
         const reader = new FileReader();
@@ -40,7 +40,7 @@ class Upload extends Component {
     
       };
       render() {
-        const currentImage = this.state
+        const {currentImage} = this.state
         return(
       
           <VStack 
@@ -48,16 +48,30 @@ class Upload extends Component {
           // divider={<Divider borderColor="gray.200" />}
           width="100%"
           align="stretch"
-          backgroundColor="#4e5250"
+          backgroundColor="#225e3d"
           padding="20px"
+          borderBottomColor="#3CB371"
+          borderBottomWidth="4px"
           >
             <Center h="100%" color="white">
-            <Heading as="h3" size="lg">
+            <Heading as="h4" size="lg">
                 Upload to CELOGram
             </Heading>
             </Center>
-            <Box>
-              <img src={currentImage} alt="" id="img" className="img" />
+            <Box
+            width="5%"
+            borderRadius="10px"
+            padding="3px"
+            borderColor="#FEE09D"
+            backgroundColor="yellow"
+            >
+            <Image
+            boxSize="50px"
+            // objectFit="cover"
+            src={currentImage}
+            alt="" 
+            id="img"
+            />
             </Box>
             <form style={{ width: "100%" }}
               onSubmit={(event) => {
@@ -67,11 +81,12 @@ class Upload extends Component {
             >
               <HStack width="70%" spacing={3} alignItems="flex-start">
                 
-                <Stack
+                {/* <VStack
                       cursor="pointer"
+                      alignItems="flex-end"
                       as="label"
                       htmlFor="image-input"
-                      px="25px"
+                      px="15px"
                       width="15%"
                       borderRadius="10px"
                       height="50px"
@@ -80,50 +95,41 @@ class Upload extends Component {
                       backgroundColor="#3CB371"
                       borderColor="#FEE09D"
                       textColor="#FEE09D"
-                    >
+                      justifyContent="center"
+                    > */}
+                      {/* <VStack height="100%" width="100%" justifyContent="center" alignItems="flex-start"> */}
                       <input
                 //   onChange={(e) => handleImage(e)}
                         name="image-upload"
                         type="file"
-                        // style={{ display: "none" }}
+                        style={{ color: 'white', background: '#3CB371' }}
                         id="input"
                         accept=".jpg, .jpeg, .png, .bmp, .gif"
                         onInput={this.props.captureFile} 
                         onChange={this.imageHandler}
                       />
-                      <VStack height="100%" width="100%" justifyContent="center">
-                        <TiPlus style={{ fill: "#FEE09D" }} />
-                        <Text fontWeight="600" color="#FEE09D">
+                        {/* <TiPlus style={{ fill: "#FEE09D" }} /> */}
+                        {/* <Text fontWeight="400" color="#FEE09D">
                           Image
-                        </Text>
+                        </Text> */}
                         
-                      </VStack>
-                </Stack>
-                {/* <Box
-                  cursor="pointer"
-                  as="label"
-                  htmlFor="image-input"
-                  px="25px"
-                  width="55%"
-                  borderRadius="10px"
-                  height="50px"
-                  borderWidth="4px"
-                  borderStyle="solid"
-                  backgroundColor="white"
-                  borderColor="#FEE09D"
-                  textColor="#FEE09D"
-                > */}
-                  <Stack width="55%" backgroundColor="white" placeholder="Media Description" borderWidth="4px" borderColor="#FEE09D" textColor="#3CB371" ><input
+                      {/* </VStack> */}
+                {/* </VStack> */}
+                  <Stack width="70%" backgroundColor="white" placeholder="Media Description" borderWidth="4px" borderColor="#FEE09D" textColor="#3CB371" >
+                    <input
                         id="imageDescription"
                         type="text"
                         // style={{ display: "none" }}
                         style={{ border: "none" }}
                         ref={(input) => { this.imageDescription = input }}
                         className="form-control"
-                        placeholder="Enter Image Caption"
+                        placeholder="Enter Image Description"
                         required />
                 </Stack>
+                <Stack width="10%" backgroundColor="#3CB371" placeholder="Media Description" borderWidth="3px" borderColor="#FEE09D" textColor="#FEE09D" borderRadius="5px" ><button type="submit">Post</button></Stack>
+
             </HStack>
+            
             </form>
             
 
