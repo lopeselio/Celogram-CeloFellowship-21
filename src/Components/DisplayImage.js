b608fade5e1e575358a3f10467db4d30bf65978d
lopeselio <lopeselio@gmail.com>
2021-08-16 19:37:03 +0530

    ternary for display image

--- src/Components/DisplayImage.js
+++ src/Components/DisplayImage.js
@@ -1,39 +1,75 @@
-import React, { Component, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Web3 from "web3";
 import {
     Center,
-    Grid,
     VStack,
-    Heading,
-    InputGroup,
-    FormControl,
     HStack,
     Image,
     Skeleton,
-    Spinner,
     Box,
-    // StackDivider,
     Stack,
-    Text,
-    Divider,
     Button,
-    FormLabel,
-    Input,
-    FormHelperText,
-    Textarea,
-    InputRightAddon,
-    SkeletonText,
     Badge,
-    StarIcon,
     Tag,
     Avatar,
     TagLabel
   } from "@chakra-ui/react";
 import '../Pages/Browse.css'
+import svgAvatarGenerator from "../utils/avatar";
+import FundPostABI from '../abis/FundPost.json'
+const ContractKit = require("@celo/contractkit")
+let kit
+
+
 
 function DisplayImage({ currentAccount }) {
 
     const [tipAm, setTipAm] = useState('2')
+    const [ isMetamaskInstalled, setIsMetamaskInstalled ] = useState(false);
+    const [ avatar, setAvatar ] = useState(undefined);
+    const [selectedImg, setSelectedImg] = useState(null);
+    const [images, setImages] = useState([]);
+    const [ isLoading, setIsLoading ] = useState(false);
+    const [FundPost, setFundPost] = useState(null);
+
+
+    
+    useEffect(() => {
+        if(window.celo && currentAccount){
+        //   setChainId(window.celo.networkVersion);
+          init();
+          
+        //   window.celo.on("accountsChanged", (accounts) => {
+        //     setCurrentAccount(accounts[0]);
+        //   })
+    
+        //   window.celo.on("chainChanged", (newChainId) => {
+        //     setChainId(newChainId.substr(-1, 1));
+        //   })
+        }
+      }, [currentAccount]);
+
+      const init = async () => {
+        const web3 = new Web3(window.celo);
+        kit = ContractKit.newKitFromWeb3(web3);
+        const networkId = await kit.web3.eth.net.getId();
+        const networkData = FundPostABI.networks[networkId];
+        let FundPostContractObj = new web3.eth.Contract(FundPostABI.abi, networkData.address);
+        setFundPost(FundPostContractObj);
+        // const ipfs = create({
+        //   host: "ipfs.infura.io",
+        //   port: 5001,
+        //   protocol: "https",
+        // });
+        // const ipfs = create({ host: 'localhost', port: '5001', protocol: 'http' })
+        
+        // const ipfs = create("http://localhost:5001/");
+        // orbitdb = await OrbitDb.createInstance(ipfs);
+        // db = await orbitdb.docs("niftysubs");
+        // pubsub = new IPFSpubsub(ipfs, "niftysubs");
+        // subscribeToTopic();
+        // initDb();
+    }
     // state = {
     //     tipAm: '2'
     // }
@@ -42,6 +78,26 @@ function DisplayImage({ currentAccount }) {
         // const am = e.target.value
         setTipAm(e.target.value)
     };
+    const tipImageOwner = (id, tipAmount) => {
+        // this.setState({ loading: true })
+        setIsLoading(true)
+        FundPost.methods.tipImageOwner(id).send({ from: currentAccount, value: tipAmount }).on('transactionHash', (hash) => {
+          // this.setState({ loading: false })
+          setIsLoading(false)
+        })
+    }
+
+    useEffect(() => {
+        if(window.celo){
+            setIsMetamaskInstalled(true); 
+            let svg = svgAvatarGenerator(currentAccount, {dataUri: true});
+            setAvatar(svg);
+        }
+        else{
+            setIsMetamaskInstalled(false);
+        }
+        
+    }, [currentAccount])
     
 
     // render() {
@@ -75,6 +131,7 @@ function DisplayImage({ currentAccount }) {
                 </Button>
                 </Stack>
                 <VStack align="center" direction="row" spacing={10} overflowY="auto" height="575px">
+                    {currentAccount ? 
                 <Center>
                 <Box padding="8px" maxW="lg" borderWidth="4px" borderRadius="lg" borderColor="#FEE09D" overflow="hidden" backgroundColor="#3cb371">
                     <Box
@@ -87,7 +144,18 @@ function DisplayImage({ currentAccount }) {
                         backgroundColor="FEE09D"
                         ml="2"
                     >
-                    Posted by 0x123456
+                    <Tag
+                        size="lg"
+                        borderRadius="full"
+                        // ml={3}
+                        // mr={-2}
+                        background="#FEE09D"
+                    >
+                        <TagLabel color="#4d8a68">Posted by {`${currentAccount.substr(0,6)}...${currentAccount.substr(-4)}`}
+                        </TagLabel>
+                        <Avatar borderStyle="solid" borderColor="#37CE81" borderWidth="2px" padding="1px" mr="-13px" ml={4} size="sm" bg="transparent" src={avatar} />
+                    </Tag>
+
                     </Box>
                     <Image borderRadius="lg" src={property.imageUrl} alt={property.imageAlt} />
 
@@ -152,85 +220,9 @@ function DisplayImage({ currentAccount }) {
                         </Box>
                    {/* </Box> */}
                     </Box>
-                    </Center>
-                    <Center>
-                    <Box padding="8px" maxW="lg" borderWidth="4px" borderRadius="lg" borderColor="#FEE09D" overflow="hidden" backgroundColor="#3cb371">
-                    <Box
-                        color="gray.500"
-                        fontWeight="semibold"
-                        letterSpacing="wide"
-                        fontSize="xs"
-                        textTransform="uppercase"
-                        padding="5px"
-                        backgroundColor="FEE09D"
-                        ml="2"
-                    >
-                    Posted by {currentAccount} 0x123456
-                    </Box>
-                    <Image borderRadius="lg" src={property.imageUrl} alt={property.imageAlt} />
-
-                     <Box p="6">
-                        <Box d="flex" alignItems="baseline">
-                        <Badge borderRadius="full" px="2" backgroundColor="#FEE09D">
-                            DESCRIPTION
-                        </Badge>
-                        <Box
-                            color="white"
-                            fontWeight="bold"
-                            letterSpacing="wide"
-                            fontSize="s"
-                            // textTransform="uppercase"
-                            ml="2"
-                            backgroundColor="#4d8a68"
-                            borderRadius="5px"
-                            padding="6px"
-                        >
-                            Hey there it is me Elio. I am from India. I am learning Blockchain development
-                        </Box>
-                        </Box>
-                    </Box>
-                    <HStack>
-                    <Tag size="lg" colorScheme="teal" borderRadius="full">
-                        <Avatar
-                            src="https://assets.coingecko.com/coins/images/11090/small/icon-celo-CELO-color-500.png?1592293590"
-                            size="xs"
-                            name="cUSD"
-                            ml={-1}
-                            mr={2}
-                        />
-                        <TagLabel color="#4d8a68" >2</TagLabel>
-                    </Tag>
-                    <Stack width="40%" backgroundColor="white" placeholder="Media Description" borderWidth="4px" borderColor="#FEE09D" textColor="#3CB371" >
-                    <input
-                        type="text"
-                        style={{width:"100px"}} 
-                        className="form-control"
-                        // onChange={this.setTip}
-                        placeholder="cUSD Tip"
-                        padding="5px"
-                        required />
-                    </Stack>
-                    <Stack width="20%" variant="ghost" backgroundColor="#FEE09D" placeholder="Media Description" borderWidth="3px" borderColor="#4d8a68" textColor="#3CB371" borderRadius="5px" ><button type="submit">TIP cUSD</button></Stack>
-                    </HStack>
-
-                       
-
-                       {/*<Box d="flex" mt="2" alignItems="center">
-                         {Array(5)
-                            .fill("")
-                            .map((_, i) => (
-                            <StarIcon
-                                key={i}
-                                color={i < property.rating ? "teal.500" : "gray.300"}
-                            />
-                            ))}
-                        <Box as="span" ml="2" color="gray.600" fontSize="sm">
-                            {property.reviewCount} reviews
-                        </Box>
-                        </Box>
-                   {/* </Box> */}
-                    </Box>
-                    </Center>
+                    </Center>:<div>Hey There</div>
+}
+                    
                     </VStack>
 
             </VStack>
