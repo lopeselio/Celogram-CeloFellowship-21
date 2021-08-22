e480a3b97f961cc1902c5bd53f41cee0ffad94e1
lopeselio <lopeselio@gmail.com>
2021-08-22 18:47:39 +0530

    updated config

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
-export const nftaddress = ""
-export const nftmarketaddress = ""
+export const nftaddress = "0xa377ECDFd84f918aff4d1Bf4bB7C08Fc8e63dB02"
+export const nftmarketaddress = "0xA593bB5217b9B57F0CB5b2194DBbe44331F64381"
