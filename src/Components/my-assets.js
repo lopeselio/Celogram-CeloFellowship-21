cbac383a296679d35fd11248dd6215e70bf0464a
lopeselio <lopeselio@gmail.com>
2021-08-23 12:53:32 +0530

    My Asset component

--- src/Components/my-assets.js
+++ src/Components/my-assets.js
@@ -137,21 +137,5 @@ export default function MyAssets({ currentAccount }) {
             </Grid>
             </Center>
         </VStack>
-    //   <div className="flex justify-center">
-    //     <div className="p-4">
-    //       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
-    //         {
-    //           nfts.map((nft, i) => (
-    //             <div key={i} className="border shadow rounded-xl overflow-hidden">
-    //               <img src={nft.image} className="rounded" />
-    //               <div className="p-4 bg-black">
-    //                 <p className="text-2xl font-bold text-white">Price - {nft.price} CELO</p>
-    //               </div>
-    //             </div>
-    //           ))
-    //         }
-    //       </div>
-    //     </div>
-    //   </div>
     )
   }
